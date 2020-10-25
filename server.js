5eab87a22c58139b0b1a2c9292283b1deb303e51
abdul-local <abdullah.hamzan@gmail.com>
2020-10-25 11:53:16 +0800

    update

--- server.js
+++ server.js
@@ -6,55 +6,77 @@ var Joi = require('joi');
 
 
 // ini dipakai untuk menggunakan data json untuk pada saat post
-app.use(express.json())
+app.use(express.json());
+
 const courses=[
-    { id:1,name:'abdul'},
+    {id:1,name:'abdul'},
     {id:2,name:'rodi'},
     {id:3,name:'malik'}
-]
+];
+
 app.get('/',(req,res)=>{
    res.send('hello word aja bro!')
 })
+
 
 //mengambil semua data courses
 app.get('/api/courses',(req,res)=>{
     res.send(courses);
 })
+
 // mengamil satu data berdasarkan nilai id nya
 app.get('/api/courses/:id',(req,res)=>{
-   courses.find(c=>c.id === parseInt(req.params.id));
+  const course= courses.find(c=>c.id === parseInt(req.params.id));
    if(!course){
        res.status(404).send('Data tidak ditemukan ')
    }
    res.send(course);
 })
 
-// Membuat data baru dengan method post
+
+// membuat data baru dengan method post
 app.post('/api/courses',(req,res)=>{
-
-    // buatkan aturan dengan nama varival schema
+    // kita berikan validasi data dengan library joi
     const schema=Joi.object({
-        name: Joi.string().min(3).required()
-    });
-
-    // const result=Joi.validate(req.body,schema);
+        name:Joi.string().min(3).required(),
+    })
     const result=schema.validate(req.body);
     // console.log(result);
-    // mamebuat validasi
     if(result.error){
-        // bad request 
-        res.status(400).send(result.error.details[0].message)
+       return res.status(400).send(result.error.details[0].message);
     }
+    
     const course={
-        id:courses.length +1,
+        id:courses.length+1,
         name:req.body.name
     }
     courses.push(course);
-  
-    res.send(course);
+    return res.send(courses);
+
+});
+
+// Mengupdate data berdasarkan ID
+app.put('/api/courses/:id',(req,res)=>{
+    const course=courses.find(c=>c.id == parseInt(req.params.id));
+        if(!course){
+            return res.status(404).json({msg:'data tidak ditemukan'});
+        }
+        const Schema=Joi.object({
+            name:Joi.string().min(3).required()
+
+        })
+        const result=Schema.validate(req.body);
+        if(result.error){
+            return res.status(400).send(result.error.details[0].message);
+        }
+
+        course.name=req.body.name;
+
+        return res.send(course);
 
 
 })
+
 
 // multiple paramter
 app.get('/api/posts/:yeart/:month',(req,res)=>{
